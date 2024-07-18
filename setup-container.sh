b153b909ed547b685e079e318a678b67966ce70f
albertAtPropertyMe <albertferguson@propertyme.com>
2024-07-18 11:08:12 +1000

    tweak to fix networking backend config (for now PoC)

--- setup-container.sh
+++ setup-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # setup a single node
-docker run --name scylla -d -p 9042:9042 scylladb/scylla
+# reactor backend updates the advanced socket config. Read more: https://java-driver.docs.scylladb.com/stable/manual/core/configuration/reference/README.html
+docker run --name scylla -d -p 9042:9042 scylladb/scylla --reactor-backend=epoll
