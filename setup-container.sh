96a36648b8c05728478c40449e64e437afe0a719
albertAtPropertyMe <albertferguson@propertyme.com>
2024-07-18 11:52:06 +1000

    enable persistence

--- setup-container.sh
+++ setup-container.sh
@@ -4,4 +4,7 @@
 #   this updates the advanced socket config.
 #   Read more: https://java-driver.docs.scylladb.com/stable/manual/core/configuration/reference/README.html
 #   the above readme links to their Java driver, but this was the only reference I could find that referenced roughly what I was looking for
-docker run --name scylla -d -p 9042:9042 scylladb/scylla --reactor-backend=epoll
+docker run --name scylla -d \
+    -v /scylladb:/var/lib/scylla \
+    -p 9042:9042 scylladb/scylla \
+    --reactor-backend=epoll
