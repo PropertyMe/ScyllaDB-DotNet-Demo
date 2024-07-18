a30ea0000a351a808d4d8bbdfae670b747a2e164
albertAtPropertyMe <albertferguson@propertyme.com>
2024-07-18 11:09:44 +1000

    comments

--- setup-container.sh
+++ setup-container.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # setup a single node
-# reactor backend updates the advanced socket config. Read more: https://java-driver.docs.scylladb.com/stable/manual/core/configuration/reference/README.html
+# reactor backend -
+#   this updates the advanced socket config.
+#   Read more: https://java-driver.docs.scylladb.com/stable/manual/core/configuration/reference/README.html
+#   the above readme links to their Java driver, but this was the only reference I could find that referenced roughly what I was looking for
+
 docker run --name scylla -d -p 9042:9042 scylladb/scylla --reactor-backend=epoll
